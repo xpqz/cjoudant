13c6aa73ad6ba44c65d406aa57605823c666caed
Stefan Kruger <stefan.kruger@uk.ibm.com>
2017-07-28 15:19:29 +0100

    Initial commit [WIP]

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject cjoudant "0.1.0-SNAPSHOT"
+  :description "Cjoudant: a tiny Cloudant client library for Clojure"
+  :url "https://github.com/xpqz/cjoudant"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/"}
+ :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cheshire "5.7.1"]
+                 [http-kit "2.2.0"]]
+  :main ^:skip-aot cjoudant.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/cjoudant/core_test.clj
@@ -0,0 +1,7 @@
+(ns cjoudant.core-test
+  (:require [clojure.test :refer :all]
+            [cjoudant.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
