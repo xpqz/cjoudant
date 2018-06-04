62c5776c46dddd78a7f6c207be749d783b104141
Stefan Kruger <stefan.kruger@uk.ibm.com>
2018-06-04 12:58:05 +0100

    Remove unused dependencies. Ensure config'd as lib

--- project.clj
+++ project.clj
@@ -5,8 +5,5 @@
             :url "http://www.apache.org/licenses/"}
  :dependencies [[org.clojure/clojure "1.7.0"]
                 [cheshire "5.7.1"]
-                [org.clojure/core.async "0.3.443"]
                 [http-kit "2.2.0"]]
-  :main ^:skip-aot cjoudant.client
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")
